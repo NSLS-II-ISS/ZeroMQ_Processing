22c4be259c3b2118096f6d8a223d5f365f1e489a
Julien Lhermitte <lhermitte@bnl.gov>
2018-04-20 09:54:06 -0400

    updated for QAS

--- dispatcher_response_srv.py
+++ dispatcher_response_srv.py
@@ -4,6 +4,8 @@ import socket
 context = zmq.Context()
 machine_name = socket.gethostname()
 
+LOGROOT = "/nsls2/xf07bm/users/log"
+
 ###### Set up logging.
 import logging
 import logging.handlers
@@ -12,14 +14,14 @@ logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 # Write DEBUG and INFO messages to /var/log/data_processing_worker/debug.log.
 debug_file = logging.handlers.RotatingFileHandler(
-    '/nsls2/xf08id/log/{}_data_processing_debug.log'.format(machine_name),
+    LOGROOT + '/{}_data_processing_debug.log'.format(machine_name),
     maxBytes=10000000, backupCount=9)
 debug_file.setLevel(logging.DEBUG)
 debug_file.setFormatter(formatter)
 logger.addHandler(debug_file)
 # Write INFO messages to /var/log/data_processing_worker/info.log.
 info_file = logging.handlers.RotatingFileHandler(
-    '/nsls2/xf08id/log/{}_data_processing_info.log'.format(machine_name),
+    LOGROOT + '/{}_data_processing_info.log'.format(machine_name),
     maxBytes=10000000, backupCount=9)
 info_file.setLevel(logging.INFO)
 info_file.setFormatter(formatter)
